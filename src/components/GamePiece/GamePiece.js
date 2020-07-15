aff92b9187f63334157842732865db26cad5d0db
Ashley Waldner <ashleywaldner22@gmail.com>
2020-07-15 18:19:41 -0500

    add some css

--- src/components/GamePiece/GamePiece.js
+++ src/components/GamePiece/GamePiece.js
@@ -2,12 +2,12 @@
 import React from 'react'
 import { Col, CardPanel } from 'react-materialize';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import './GamePiece.css'
+import './GamePiece.css'
 
 const GamePiece = props => (
-    <Col s={12} m={4} l={3}>
+    <Col s={12} m={4} l={3} className="card">
         <CardPanel onClick={() => props.clickHandler(props.icon.iconName)} className={"hoverable teal lighten-4 black-text center" + (props.correct === false ? " shake" : "")}>
-            <FontAwesomeIcon icon={props.icon} />
+            <FontAwesomeIcon className="icon" icon={props.icon} />
         </CardPanel>
     </Col>
 )
